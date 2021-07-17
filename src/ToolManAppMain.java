02a7bc54b81d95237f645dfc7f18fd3db4aebe1d
TadasSa <t.sabaitis@gmail.com>
2021-07-17 13:50:34 +0300

    Initial commit

---
+++ src/ToolManAppMain.java
@@ -0,0 +1,8 @@
+public class ToolManAppMain {
+    public static void main(String[] args) {
+        SayHello();
+    }
+    public static void SayHello() {
+        System.out.println("Hello Tool Management App");
+    }
+}
